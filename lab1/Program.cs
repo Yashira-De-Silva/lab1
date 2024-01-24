c79356ed13a6da2c516e13902192a83ed00681ca
Yashira-De-Silva <yashiradesilva@gmail.com>
2024-01-24 13:58:29 +0530

    Add project files.

---
+++ lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the rectangle: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the width of the rectangle: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+          
+            double area = length * width;
+
+           
+            Console.WriteLine("The area of the rectangle is:  " +area );
+            Console.ReadLine();
+        }
+    }
+}
